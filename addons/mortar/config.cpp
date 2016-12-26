3d1eab1cfb92faa09600fab521ec7f65c596e4d4
ElTyranos <pierre.cazard@gmail.com>
2016-12-26 22:26:20 +0100

    removed deprecated includes

--- addons/mortar/config.cpp
+++ addons/mortar/config.cpp
@@ -14,35 +14,17 @@ class CfgPatches {
     };
 };
 
-#include "ACE_Settings.hpp"
 #include "Extended_PreStart_EventHandlers.hpp"
 #include "Extended_PreInit_EventHandlers.hpp"
 #include "Extended_PostInit_EventHandlers.hpp"
 #include "Extended_FiredBIS_EventHandlers.hpp"
-
 
 class Mode_SemiAuto;
 class Mode_Burst;
 class Mode_FullAuto;
 class BaseSoundModeType;
 
-
-class RscOpticsText;
-class RscOpticsValue;
-class RscText;
-class RscListbox;
-class RscListNBox;
-class RscPicture;
-class RscControlsGroup;
-class ScrollBar;
-class RscActiveText;
-class RscStructuredText;
-#include "CfgCloudlets.hpp"
-#include "ifa3_GrenadeExplosion.hpp"
-#include "if_WPExplosion.hpp"
 #include "CfgAmmo.hpp"
 #include "CfgMagazines.hpp"
 #include "CfgVehicles.hpp"
 #include "CfgWeapons.hpp"
-#include "RscInGameUI.hpp"
-#include "ACE_D30_RangeTable_Dialog.hpp"

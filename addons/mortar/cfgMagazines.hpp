6241db9f499963b21702649724637ea752612491
ElTyranos <pierre.cazard@gmail.com>
2017-05-25 16:42:34 +0200

    ~ Improved: mortar rounds UI

--- addons/mortar/cfgMagazines.hpp
+++ addons/mortar/cfgMagazines.hpp
@@ -8,7 +8,7 @@ class CfgMagazines
 		model = "\WW2\Assets_m\Weapons\Ammoboxes_m\mortars\WW2_shell80.p3d";
 		hiddenSelections[] = {"camo1"};
 		hiddenSelectionsTextures[] = {"WW2\Assets_t\Weapons\Ammoboxes_t\Mortars\shell80_he_co.paa"};
-		// picture = "\z\ifa3_comp_ace\addons\mortar\ui\ui_he_round.paa";
+		picture = "\WW2\Assets_t\Weapons\Equipment_t\Magazines\Mortars\M_80_HE_ca.paa";
 		ammo = "LIB_Sh_82_HE";
 	};
 	class LIB_1Rnd_82mm_Mo_Smoke: ACE_1Rnd_82mm_Mo_Smoke
@@ -16,14 +16,14 @@ class CfgMagazines
 		model = "\WW2\Assets_m\Weapons\Ammoboxes_m\mortars\WW2_shell80.p3d";
 		hiddenSelections[] = {"camo1"};
 		hiddenSelectionsTextures[] = {"WW2\Assets_t\Weapons\Ammoboxes_t\Mortars\shell80_smoke_co.paa"};	
-		// picture = "\z\ifa3_comp_ace\addons\mortar\ui\ui_smoke_round.paa";
+		picture = "\WW2\Assets_t\Weapons\Equipment_t\Magazines\Mortars\M_80_Smoke_ca.paa";
 	};
 	class LIB_1Rnd_82mm_Mo_Illum: ACE_1Rnd_82mm_Mo_Illum
 	{
 		model = "\WW2\Assets_m\Weapons\Ammoboxes_m\mortars\WW2_shell80.p3d";
 		hiddenSelections[] = {"camo1"};
 		hiddenSelectionsTextures[] = {"WW2\Assets_t\Weapons\Ammoboxes_t\Mortars\shell80_illum_co.paa"};		
-		// picture = "\z\ifa3_comp_ace\addons\mortar\ui\ui_illum_round.paa";
+		picture = "\WW2\Assets_t\Weapons\Equipment_t\Magazines\Mortars\M_80_Illum_ca.paa";
 	};
 	class LIB_1Rnd_81mm_Mo_HE: ACE_1Rnd_82mm_Mo_HE
 	{
@@ -31,7 +31,7 @@ class CfgMagazines
 		model = "\WW2\Assets_m\Weapons\Ammoboxes_m\mortars\WW2_shell80.p3d";
 		hiddenSelections[] = {"camo1"};
 		hiddenSelectionsTextures[] = {"WW2\Assets_t\Weapons\Ammoboxes_t\Mortars\shell80_he_co.paa"};
-		// picture = "\z\ifa3_comp_ace\addons\mortar\ui\ui_he_round.paa";
+		picture = "\WW2\Assets_t\Weapons\Equipment_t\Magazines\Mortars\M_80_HE_ca.paa";
 		ammo = "LIB_Sh_81_HE";
 	};
 	class LIB_1Rnd_81mm_Mo_Smoke: ACE_1Rnd_82mm_Mo_Smoke
@@ -40,7 +40,7 @@ class CfgMagazines
 		model = "\WW2\Assets_m\Weapons\Ammoboxes_m\mortars\WW2_shell80.p3d";
 		hiddenSelections[] = {"camo1"};
 		hiddenSelectionsTextures[] = {"WW2\Assets_t\Weapons\Ammoboxes_t\Mortars\shell80_smoke_co.paa"};		
-		// picture = "\z\ifa3_comp_ace\addons\mortar\ui\ui_smoke_round.paa";
+		picture = "\WW2\Assets_t\Weapons\Equipment_t\Magazines\Mortars\M_80_Smoke_ca.paa";
 	};
 	class LIB_1Rnd_81mm_Mo_Illum: ACE_1Rnd_82mm_Mo_Illum
 	{
@@ -48,7 +48,7 @@ class CfgMagazines
 		model = "\WW2\Assets_m\Weapons\Ammoboxes_m\mortars\WW2_shell80.p3d";
 		hiddenSelections[] = {"camo1"};
 		hiddenSelectionsTextures[] = {"WW2\Assets_t\Weapons\Ammoboxes_t\Mortars\shell80_illum_co.paa"};		
-		// picture = "\z\ifa3_comp_ace\addons\mortar\ui\ui_illum_round.paa";
+		picture = "\WW2\Assets_t\Weapons\Equipment_t\Magazines\Mortars\M_80_Illum_ca.paa";
 	};
 	class LIB_1Rnd_60mm_Mo_HE: ACE_1Rnd_82mm_Mo_HE
 	{
@@ -56,7 +56,7 @@ class CfgMagazines
 		model = "\WW2\Assets_m\Weapons\Ammoboxes_m\mortars\WW2_shell60.p3d";
 		hiddenSelections[] = {"camo1"};
 		hiddenSelectionsTextures[] = {"WW2\Assets_t\Weapons\Ammoboxes_t\Mortars\shell60_he_co.paa"};
-		// picture = "\z\ifa3_comp_ace\addons\mortar\ui\ui_he_round.paa";
+		picture = "\WW2\Assets_t\Weapons\Equipment_t\Magazines\Mortars\M_60_HE_ca.paa";
 		ammo = "LIB_Sh_60_HE";
 		mass = 30;
 	};
@@ -66,7 +66,7 @@ class CfgMagazines
 		model = "\WW2\Assets_m\Weapons\Ammoboxes_m\mortars\WW2_shell60.p3d";
 		hiddenSelections[] = {"camo1"};
 		hiddenSelectionsTextures[] = {"WW2\Assets_t\Weapons\Ammoboxes_t\Mortars\shell60_smoke_co.paa"};		
-		// picture = "\z\ifa3_comp_ace\addons\mortar\ui\ui_smoke_round.paa";
+		picture = "\WW2\Assets_t\Weapons\Equipment_t\Magazines\Mortars\M_60_Smoke_ca.paa";
 		mass = 30;
 	};
 	class LIB_1Rnd_60mm_Mo_Illum: ACE_1Rnd_82mm_Mo_Illum
@@ -75,7 +75,7 @@ class CfgMagazines
 		model = "\WW2\Assets_m\Weapons\Ammoboxes_m\mortars\WW2_shell60.p3d";
 		hiddenSelections[] = {"camo1"};
 		hiddenSelectionsTextures[] = {"WW2\Assets_t\Weapons\Ammoboxes_t\Mortars\shell60_illum_co.paa"};
-		// picture = "\z\ifa3_comp_ace\addons\mortar\ui\ui_illum_round.paa";
+		picture = "\WW2\Assets_t\Weapons\Equipment_t\Magazines\Mortars\M_60_Illum_ca.paa";
 		mass = 30;
 	};
 };

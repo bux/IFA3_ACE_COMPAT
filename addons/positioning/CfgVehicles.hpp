5d5a8d495c17eb5fb298a95c4e72f1c74a68a559
ElTyranos <pierre.cazard@gmail.com>
2017-05-25 16:43:02 +0200

    ~ Improved: formatting

--- addons/positioning/CfgVehicles.hpp
+++ addons/positioning/CfgVehicles.hpp
@@ -6,13 +6,13 @@ class CfgVehicles {
 		
 		class ACE_SelfActions {
 			class ACE_IFa3_right {
-				displayName = $STR_ifa3_ace_pos_right;
+				displayName = "$STR_ifa3_ace_pos_right";
 				condition = "alive _target";
 				statement = "_target call ACE_pos_fnc_rotationRight";
 				icon = "\z\ifa3_comp_ace\addons\positioning\ui\right.paa";
 			};	
 			class ACE_IFa3_left{
-				displayName = $STR_ifa3_ace_pos_left;
+				displayName = "$STR_ifa3_ace_pos_left";
 				condition = "alive _target";
 				statement = "_target call ACE_pos_fnc_rotationLeft";
 				icon = "\z\ifa3_comp_ace\addons\positioning\ui\left.paa";
@@ -23,13 +23,13 @@ class CfgVehicles {
 		
 		class ACE_SelfActions {
 			class ACE_IFa3_right {
-				displayName = $STR_ifa3_ace_pos_right;
+				displayName = "$STR_ifa3_ace_pos_right";
 				condition = "alive _target";
 				statement = "_target call ACE_pos_fnc_rotationRight";
 				icon = "\z\ifa3_comp_ace\addons\positioning\ui\right.paa";
 			};	
 			class ACE_IFa3_left{
-				displayName = $STR_ifa3_ace_pos_left;
+				displayName = "$STR_ifa3_ace_pos_left";
 				condition = "alive _target";
 				statement = "_target call ACE_pos_fnc_rotationLeft";
 				icon = "\z\ifa3_comp_ace\addons\positioning\ui\left.paa";
@@ -42,13 +42,13 @@ class CfgVehicles {
 	class LIB_Mortar_ACE : StaticMortar {
 		class ACE_SelfActions {
 			class ACE_IFa3_right {
-				displayName = $STR_ifa3_ace_pos_right;
+				displayName = "$STR_ifa3_ace_pos_right";
 				condition = "alive _target";
 				statement = "_target call ACE_pos_fnc_rotationRight";
 				icon = "\z\ifa3_comp_ace\addons\positioning\ui\right.paa";
 			};	
 			class ACE_IFa3_left{
-				displayName = $STR_ifa3_ace_pos_left;
+				displayName = "$STR_ifa3_ace_pos_left";
 				condition = "alive _target";
 				statement = "_target call ACE_pos_fnc_rotationLeft";
 				icon = "\z\ifa3_comp_ace\addons\positioning\ui\left.paa";

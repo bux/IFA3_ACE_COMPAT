b3e634ea17d100cdd5a746500f7fa7c21742ed0f
kju <pvpscene@web.de>
2017-12-06 13:52:40 +0100

    ~ Updated: strings.

--- addons/mortar/CfgWeapons.hpp
+++ addons/mortar/CfgWeapons.hpp
@@ -6,7 +6,7 @@ class CfgWeapons
     };
 	class LIB_BM37_ACE: mortar_82mm {
 		author = "IFA3 Team";
-		displayname = "$STR_LIB_DN_BM37";
+		displayname = "$STR_DN_LIB_BM37";
 		magazines[] = {"LIB_1Rnd_82mm_Mo_HE","LIB_1Rnd_82mm_Mo_Smoke","LIB_1Rnd_82mm_Mo_Illum"};
 		modes[] = {"Single1","Single2","Single3"};
 		reloadTime = 0.5;
@@ -17,7 +17,7 @@ class CfgWeapons
 	};
 	class LIB_GrWr34_ACE: mortar_82mm {
 		author = "IFA3 Team";
-		displayname = "$STR_LIB_DN_GRWR34";
+		displayname = "$STR_DN_LIB_GRWR34";
 		magazines[] = {"LIB_1Rnd_81mm_Mo_HE","LIB_1Rnd_81mm_Mo_Smoke","LIB_1Rnd_81mm_Mo_Illum"};
 		modes[] = {"Single1","Single2","Single3"};
 		reloadTime = 0.5;
@@ -28,7 +28,7 @@ class CfgWeapons
 	};
 	class LIB_M2_60_ACE: mortar_82mm {
 		author = "IFA3 Team";
-		displayname = "$STR_LIB_DN_M2_60";
+		displayname = "$STR_DN_LIB_M2_60";
 		magazines[] = {"LIB_1Rnd_60mm_Mo_HE","LIB_1Rnd_60mm_Mo_Smoke","LIB_1Rnd_60mm_Mo_Illum"};
 		modes[] = {"Single1","Single2"};
 		reloadTime = 0.5;

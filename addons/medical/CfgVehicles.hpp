ec169422d9f897566d2f7000d1b3b60973d863b7
kerckasha <daza400@gmail.com>
2020-01-03 23:38:50 +1000

    ~ Fixed: Missing hitpoints on RA units

---
+++ addons/medical/CfgVehicles.hpp
@@ -0,0 +1,10 @@
+class CfgVehicles {
+
+  //units
+  class O_Soldier_base_F;
+  class LIB_SOV_Soldier_base: O_Soldier_base_F {
+      class HitPoints {
+          ADD_ACE_HITPOINTS(1,1);
+      };
+  };
+};
---
+++ addons/medical/script_component.hpp
@@ -0,0 +1,5 @@
+#define COMPONENT medical
+#include "\z\ifa3_comp_ace\addons\main\script_mod.hpp"
+
+#include "\z\ace\addons\main\script_macros.hpp"
+#include "\z\ace\addons\medical_engine\script_macros_config.hpp"

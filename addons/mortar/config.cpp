2b2187df3adc85665648329dc12c41254e0faedc
ElTyranos <pierre.cazard@gmail.com>
2020-08-16 22:54:54 +0200

    ~ Changed: required addons.

--- addons/mortar/config.cpp
+++ addons/mortar/config.cpp
@@ -5,7 +5,7 @@ class CfgPatches {
         units[] = {"LIB_GrWr34_ACE","LIB_BM37_ACE","LIB_M2_60_ACE"};
         weapons[] = {"LIB_BM37_ACE","LIB_GrWr34_ACE","LIB_M2_60_ACE"};
         requiredVersion = REQUIRED_VERSION;
-        requiredAddons[] = {"A3_Weapons_F","A3_Static_F","LIB_weapons","ace_interaction","ace_mk6mortar","WW2_Assets_c_Vehicles_StaticWeapons_c","WW2_Assets_c_Weapons_Ammoboxes_c"};
+        requiredAddons[] = {"A3_Weapons_F","A3_Static_F","LIB_weapons","ace_interaction","ace_mk6mortar","WW2_Assets_c_Vehicles_StaticWeapons_c","WW2_Assets_c_Vehicles_Weapons_c","WW2_Assets_c_Weapons_Ammoboxes_c"};
         author = CSTRING(Team);
         authors[] = { "WOG", "El Tyranos" };
         authorUrl = CSTRING(Url);

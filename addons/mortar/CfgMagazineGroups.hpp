0848c82d66843457f61d842b8abc4833a1519479
kerckasha <daza400@gmail.com>
2020-08-17 22:44:47 +1000

    ~ Adjusted: Magazine groups and magazines to use default classes rather than special ace ones

--- addons/mortar/CfgMagazineGroups.hpp
+++ addons/mortar/CfgMagazineGroups.hpp
@@ -1,10 +1,17 @@
 class ace_csw_groups { // Ammo that can be loaded into CSW's
 	///// 82 mm ammo (for BM 37)
 	class LIB_1Rnd_82mm_Mo_HE { // The magazine which the player can place into their inventory
-		LIB_1Rnd_82mm_Mo_HE = 1; // Magazine that is loaded into the weapon as per CfgWeapons >> weapon >> magazines
+		LIB_1rnd_82mmHE_BM37 = 1; // Magazine that is loaded into the weapon as per CfgWeapons >> weapon >> magazines
+	};
+	class LIB_1rnd_82mmHE_BM37 { // The magazine which the player can place into their inventory
+		LIB_1rnd_82mmHE_BM37 = 1; // Magazine that is loaded into the weapon as per CfgWeapons >> weapon >> magazines
+		LIB_8Rnd_82mmHE_BM37 = 1;
 	};
 	class LIB_1Rnd_82mm_Mo_Smoke { // The magazine which the player can place into their inventory
-		LIB_1Rnd_82mm_Mo_Smoke = 1; // Magazine that is loaded into the weapon as per CfgWeapons >> weapon >> magazines
+		LIB_82mm_BM37_SmokeShell = 1; // Magazine that is loaded into the weapon as per CfgWeapons >> weapon >> magazines
+	};
+	class LIB_82mm_BM37_SmokeShell { // The magazine which the player can place into their inventory
+		LIB_82mm_BM37_SmokeShell = 1; // Magazine that is loaded into the weapon as per CfgWeapons >> weapon >> magazines
 	};
 	class LIB_1Rnd_82mm_Mo_Illum { // The magazine which the player can place into their inventory
 		LIB_1Rnd_82mm_Mo_Illum = 1; // Magazine that is loaded into the weapon as per CfgWeapons >> weapon >> magazines
@@ -13,10 +20,17 @@ class ace_csw_groups { // Ammo that can be loaded into CSW's
 
 	///// 81 mm ammo (for GrWr 34)
 	class LIB_1Rnd_81mm_Mo_HE {
-		LIB_1Rnd_81mm_Mo_HE = 1;
+		LIB_1rnd_81mmHE_GRWR34 = 1;
+	};
+	class LIB_1rnd_81mmHE_GRWR34 {
+		LIB_1rnd_81mmHE_GRWR34 = 1;
+		LIB_8rnd_81mmHE_GRWR34 = 1;
 	};
 	class LIB_1Rnd_81mm_Mo_Smoke {
-		LIB_1Rnd_81mm_Mo_Smoke = 1;
+		LIB_81mm_GRWR34_SmokeShell = 1;
+	};
+	class LIB_81mm_GRWR34_SmokeShell {
+		LIB_81mm_GRWR34_SmokeShell = 1;
 	};
 	class LIB_1Rnd_81mm_Mo_Illum {
 		LIB_1Rnd_81mm_Mo_Illum = 1;
@@ -25,10 +39,17 @@ class ace_csw_groups { // Ammo that can be loaded into CSW's
 
 	///// 60 mm ammo (for M2)
 	class LIB_1Rnd_60mm_Mo_HE {
-		LIB_1Rnd_60mm_Mo_HE = 1;
+		LIB_1rnd_60mmHE_M2 = 1;
+	};
+	class LIB_1rnd_60mmHE_M2 {
+		LIB_1rnd_60mmHE_M2 = 1;
+		LIB_8rnd_60mmHE_M2 = 1;
 	};
 	class LIB_1Rnd_60mm_Mo_Smoke {
-		LIB_1Rnd_60mm_Mo_Smoke = 1;
+		LIB_60mm_M2_SmokeShell = 1;
+	};
+	class LIB_60mm_M2_SmokeShell {
+		LIB_60mm_M2_SmokeShell = 1;
 	};
 	class LIB_1Rnd_60mm_Mo_Illum {
 		LIB_1Rnd_60mm_Mo_Illum = 1;
